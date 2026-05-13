14141efc389dd02091b4cc783712bb62cd9c795a
seda717 <tomasaguilarmarcos@gmail.com>
2026-05-13 19:08:24 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P0, 180)
+    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P0, -90)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P0, -250)
+    basic.pause(500)
+    pins.servoWritePin(AnalogPin.P0, -90)
+})
+pins.servoWritePin(AnalogPin.P0, -90)
 basic.forever(function () {
 	
 })
